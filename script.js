62ac4956de36810bdc178fb9449bf31a68e844e7
NiKole Maxwell <nvymaxwell@outlook.com>
2017-08-10 19:53:21 -0400

    Add Settings.json/initial script

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 /*/      Script for NVY BOT     /*/
-/*/             By Nvy.io           /*/  
-
-/*/   Discord.jd v 11.101      /*/
+/*/             By Nvy.io             /*/  
+/*/       Discord.jd v 11.101      /*/
 
 
 /*
   Basic needs: A bot that welcomes new guild members when they join
 */
+// Reminder - client = nvybot
 
+const Discord = require('discord.js');
+const nvybot = new Discord.Client(); //client
+const token = require('./settings.json');
+
+nvybot.on('ready', () => {
+  console.log('Prepare to be nvy\'d!');
+});
+
+nvybot.on('message', message => {
+  if (message.content === 'test') {
+    message.reply('Ready to work baby!');
+  }
+});
+
+nvybot.login('token');
